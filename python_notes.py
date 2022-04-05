48a8a3889cb51043c202dabc1f20d95d478f3c45
shubham_11 <shubhamganeshsonawane11@gmail.com>
2022-04-05 16:12:47 +0530

    Adding Python notes

---
+++ python_notes.py
@@ -0,0 +1,72 @@
+# from curses import nl
+
+
+# print("Hello world")
+
+# result = 10+10
+# print(result)
+
+# string1 = "string1"+"string1"
+# print(string1)
+
+#u are boy = boy
+#u are not boy 
+# boy = 1
+# if boy == 1 :
+#     print("i am boy!")
+# else :
+#     print("u are not boy")
+
+# list1 = ['cake','cookie']
+# list1.remove('cake')
+# print(list1)
+
+# list2 = [1,4,3,6,2]
+# list2.sort()
+# print(list2)#1 2 3 4 6
+
+# list3 = ['shubham','sonawane']
+# list3.append('ganesh')
+# print(list3)
+
+# list = 1
+# while list<=5:
+#     print(list)
+#     list=list+1 #i+=1 #i++ ++i
+
+# i=1
+# for i in range(5):
+#     print(i)
+    
+# string = "i Am boy"
+# print(string.lower())
+
+# string = " your"
+# print(string.upper())
+
+# string = "i Am boy"
+# print(string.title())
+
+# string = "i Am boy"
+# print(string.find('o'))
+
+# string = " shubham "
+# print(string.replace('m','u'))
+
+# s = "".join(["i","am","boy"])
+# print(s)
+
+#//dictionary
+# string1 : 1
+
+# from msilib.schema import Directory
+
+# directory1 = {"sting1":1,"string2":2}
+# directory2 = {"sting13":13,"string23":23}
+# directory1.update(directory2)
+
+# print(directory1)
+
+
+
+    
