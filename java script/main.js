19d581f8c5257fee16b795d8338f4e79f43fe87e
shubham_11 <shubhamganeshsonawane11@gmail.com>
2022-04-20 16:16:31 +0530

    Adding js file

---
+++ java script/main.js
@@ -0,0 +1,43 @@
+// Js overviwe
+// js indroduction
+// js writing Code 
+// Js displaying output
+// js statements
+// js Syntax
+// js comments 
+// js variables
+// js data type 
+// js operators
+// ja funtions
+// js object 
+// ja string 
+// js string mehtods / funtions
+// js Array
+// js loops for/while/ for of/
+// js conditional statements
+// js break and continue
+// js timeout and setInterval
+// js variable scope 
+// js strick mode 
+
+// // javascript Advanced
+
+// js Dates 
+// js data formats
+// ja dateget mehtods
+
+// js this keyword
+// js Arrow function 
+// js class
+
+// js JSON
+// js forms 
+// js form inputs 
+
+// js Reserved keyword
+// js function invocation
+
+
+
+//--1> Js overviwe
+
